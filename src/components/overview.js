37798a9af4383238ec04b1c950a0cdb50bbb53dd
Adrian Oprea <adrian@oprea.rocks>
2019-11-15 01:12:04 +0200

    feature(overview): Calculate stats on the fly

--- src/components/overview.js
+++ src/components/overview.js
@@ -1,14 +1,78 @@
-import React from "react"
+import React, { useState } from "react"
 import NothingHere from "./nothing-here"
+const DefaultValues = {
+    kitchen: {
+      cold: 0,
+      hot: 0,
+    },
+    bathroom: {
+      cold: 0,
+      hot: 0,
+    },
+    total: {
+      cold: 0,
+      hot: 0,
+    }
+  };
 
 const Overview = ({ records }) => {
+  const calculateStats = (v) => {
+    const lastMonthRecord = records[records.length - 1]
+
+    const kitchenHot = parseFloat(lastMonthRecord.kitchen.hot).toFixed(3)
+    const kitchenCold = parseFloat(lastMonthRecord.kitchen.cold).toFixed(3)
+    const bathroomHot = parseFloat(lastMonthRecord.bathroom.hot).toFixed(3)
+    const bathroomCold = parseFloat(lastMonthRecord.bathroom.cold).toFixed(3)
+
+    const previousMonthRecord = records[records.length - 2]
+    const previousMonthRecordKitchenHot = parseFloat(
+      previousMonthRecord.kitchen.hot
+    ).toFixed(3)
+    const previousMonthRecordKitchenCold = parseFloat(
+      previousMonthRecord.kitchen.cold
+    ).toFixed(3)
+    const previousMonthRecordBathroomHot = parseFloat(
+      previousMonthRecord.bathroom.hot
+    ).toFixed(3)
+    const previousMonthRecordBathroomCold = parseFloat(
+      previousMonthRecord.bathroom.cold
+    ).toFixed(3)
+
+    return {
+      kitchen: {
+        cold: kitchenCold - previousMonthRecordKitchenCold,
+        hot: kitchenHot - previousMonthRecordKitchenHot
+      },
+      bathroom: {
+        cold: bathroomCold - previousMonthRecordBathroomCold,
+        hot: bathroomHot - previousMonthRecordBathroomHot
+      },
+      total: {
+        cold: lastMonthRecord.total.cold,
+        hot: lastMonthRecord.total.hot,
+      }
+    }
+  }
+
   const noRecords = !records || records.length === 0
+  let initialState = DefaultValues;
+  if (!noRecords) {
+    const stats = calculateStats();
+    Object.assign(initialState, stats);
+  }
+
+  const [totalsByConsumption, setTotalsByConsumption] = useState(initialState);
+
+
   return (
-    <div className="my-20 w-full p-20 bg-blue-200">
+    <div className="my-20 w-full p-20 bg-blue-100">
       <div className="max-w-5xl m-auto font-mono">
         <div className="text-center">
           <div className="mb-12 text-center">
-            <select className="align-middle text-4xl mr-4" disabled={noRecords}>
+            <select onChange={(v) => {
+              const values = calculateStats();
+              setTotalsByConsumption(values);
+            }} className="align-middle text-4xl mr-4" disabled={noRecords}>
               <option value="monthly">Monthly</option>
               <option value="yearly">Yearly</option>
             </select>
@@ -31,37 +95,37 @@ const Overview = ({ records }) => {
               <tbody className="text-3xl font-bold text-blue-900">
                 <tr>
                   <td>
-                    <span aria-role="img" aria-label="Faucet emoji">
+                    <span role="img" aria-label="Faucet emoji">
                       🚰
                     </span>{" "}
-                    1.4mc
+                    {totalsByConsumption.kitchen.cold.toFixed(3)}mc
                     <br />
-                    <span aria-role="img" aria-label="Bath tub emoji">
+                    <span role="img" aria-label="Bath tub emoji">
                       🛁
                     </span>{" "}
-                    14mc
+                    {totalsByConsumption.bathroom.cold.toFixed(3)}mc
                   </td>
                   <td>
-                    <span aria-role="img" aria-label="Faucet emoji">
+                    <span role="img" aria-label="Faucet emoji">
                       🚰
                     </span>{" "}
-                    1.2mc
+                    {totalsByConsumption.kitchen.hot.toFixed(3)}mc
                     <br />
-                    <span aria-role="img" aria-label="Bath tub emoji">
+                    <span role="img" aria-label="Bath tub emoji">
                       🛁
                     </span>{" "}
-                    1.2mc
+                    {totalsByConsumption.bathroom.hot.toFixed(3)}mc
                   </td>
                   <td>
-                    <span aria-role="img" aria-label="Snowflake emoji">
+                    <span role="img" aria-label="Snowflake emoji">
                       🔵
                     </span>{" "}
-                    2.6mc
+                    {totalsByConsumption.total.cold}mc
                     <br />
-                    <span aria-role="img" aria-label="Flame emoji">
+                    <span role="img" aria-label="Flame emoji">
                       🔴
                     </span>{" "}
-                    7.11mc
+                    {totalsByConsumption.total.hot}mc
                   </td>
                 </tr>
               </tbody>
