d095663dbeee9d7033a2fc7d93c6a1dc0f36bd21
Adrian Oprea <adrian@oprea.rocks>
2019-11-15 01:12:40 +0200

    feature: Extract firebase into a separate module / fetch projects from Firestore into the app

--- src/lib/use-auth.js
+++ src/lib/use-auth.js
@@ -1,21 +1,6 @@
 // Hook (use-auth.js)
 import React, { useState, useEffect, useContext, createContext } from "react"
-import * as firebase from "firebase/app"
-import "firebase/auth"
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-}
-
-if (typeof window !== "undefined") {
-  firebase.initializeApp(firebaseConfig)
-}
+import firebase from "./firebase"
 
 const authContext = createContext()
 
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,24 +1,59 @@
-import React, { Fragment, useEffect } from "react"
+import React, { Fragment, useEffect, useState } from "react"
 import { Link, navigate } from "gatsby"
 
 import Layout from "../components/layout"
-import { useAuth } from "../lib/use-auth"
 import Overview from "../components/overview"
 import IndexTable from "../components/index-table"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import bear from "../images/bear.jpg"
 import seals from "../images/seals.jpg"
+import firebase from "../lib/firebase"
+import { useAuth } from "../lib/use-auth"
 
-const DashboardPage = () => (
-  <Fragment>
-    <SEO title="Welcome to the OnTap Dashboard" />
-    <div className="max-w-5xl mx-auto font-mono mt-20 mb-0">
-      <h2 className="leading-tight tracking-wider text-2xl font-bold uppercase text-gray-700">Dashboard</h2>
-    </div>
-    <Overview />
-    <IndexTable />
-  </Fragment>
-)
+const db = firebase.firestore()
+
+const DashboardPage = () => {
+  const [records, setRecords] = useState([])
+  const auth = useAuth()
+  const { user } = auth
+
+  if (user) {
+    db.collection("records")
+      .where("userID", "==", user.uid).orderBy("createdAt", "asc")
+      .onSnapshot(querySnapshot => {
+        let userRecords = []
+        querySnapshot.forEach(doc => userRecords.push(doc.data()))
+        if (userRecords) {
+          setRecords(userRecords)
+        } else {
+          setRecords([])
+        }
+      })
+  }
+
+  const addRecord = data =>
+    db
+      .collection("records")
+      .doc()
+      .set({
+        ...data,
+        createdAt: new Date(),
+        userID: user.uid
+      })
+
+  return (
+    <Fragment>
+      <SEO title="Welcome to the OnTap Dashboard" />
+      <div className="max-w-5xl mx-auto font-mono mt-20 mb-0">
+        <h2 className="leading-tight tracking-wider text-2xl font-bold uppercase text-gray-700">
+          Dashboard
+        </h2>
+      </div>
+      <Overview records={records} />
+      <IndexTable records={records} addRecord={addRecord} />
+    </Fragment>
+  )
+}
 
 export default DashboardPage
