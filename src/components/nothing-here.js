c4595bd254f2f2f682fcc399b42879711cb94ba1
Adrian Oprea <adrian@oprea.rocks>
2019-11-14 19:54:50 +0200

    feature(nothing-here): Add empty-state component

---
+++ src/components/nothing-here.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react"
+
+const NothingHere = props => {
+  return <Fragment>
+    <h2 className="leading-tight text-5xl font-bold">Summary</h2>
+  </Fragment>
+}
+
+export default NothingHere
