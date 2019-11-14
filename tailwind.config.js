cda5e20513c0fa97e81547e85546b6d56acaedaa
Adrian Oprea <adrian@oprea.rocks>
2019-11-14 16:01:57 +0200

    chore: Whitespace

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ const defaultConfig = require("tailwindcss/defaultConfig")
 
 module.exports = Object.assign({}, defaultConfig, {
   theme: {
-      fontFamily: {
-        mono: ["'IBM Plex Mono'", ...defaultConfig.theme.fontFamily.mono],
-        sans: ["'IBM Plex Sans'", ...defaultConfig.theme.fontFamily.sans],
-      },
+    fontFamily: {
+      mono: ["'IBM Plex Mono'", ...defaultConfig.theme.fontFamily.mono],
+      sans: ["'IBM Plex Sans'", ...defaultConfig.theme.fontFamily.sans],
+    },
   },
 })
